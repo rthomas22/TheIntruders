cdf2f5f64ea0bfc74eff1485622d0ca3dae8704b
Robert Thomas <roberthomas22@gmail.com>
2023-08-16 16:09:21 -0400

    Initial Push of Project Files

---
+++ Source/TheIntruders/TheIntrudersGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TheIntrudersGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEINTRUDERS_API ATheIntrudersGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
